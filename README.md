cdec5e875e35e7d1ef240d4a3c51b3bacf5dfcf5
misostack <misostack.com@gmail.com>
2021-11-11 00:00:49 +0700

    Route Part 1

--- README.md
+++ README.md
@@ -4,7 +4,19 @@
 
 ## Wiki
 
+- [Documents](./documents/README.md)
+
+### Sitemap
+
+![Sitemap](./documents/sitemap.jpg)
+
 ### Development
+
+- Local: http://danhngon.local/
+
+```bash
+php artisan serve
+```
 
 ### Deployment
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => "PHP MVC - Bootstrap"
+    ]);
 });
+
+Route::get('/text', function(){
+    return '<h1>Route : HTML</h1>';
+});
+
+Route::get('/json', function(){
+    return json_encode(array("foo" => "bar"));
+});
+Route::get('tada', function(){
+    return json_encode(array("foo" => "bar"));
+});
+
+// 302 : Moved Temporary
+Route::redirect('/html','/text');
+// 301: Moved Permanently
+Route::redirect('/string','/text', 301);
+// 301
+Route::permanentRedirect('/old-string', '/string');
+
+// Route Parameters
+Route::get('/examples/{firstName}/{lastName}', function ($firstName, $lastName) {
+    return view('home', [
+        "title" => "Hello $firstName $lastName"
+    ]);
+});
+
+// https://laravel.com/docs/8.x/routing#redirect-routes
